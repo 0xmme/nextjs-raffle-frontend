9f6a6c6ade1ad4b5b414ef035177e078ac0781bd
mme <9083787+mme022@users.noreply.github.com>
2022-08-15 14:02:41 +0100

    added Header with web3uikit and got tailwindcss working

---
+++ components/Header.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+import { ConnectButton } from "@web3uikit/web3";
+
+const Header: NextPage = () => {
+  return (
+    <div className="p-5 border-b-2 flex flex-row">
+      <h1 className="py-4 px-4 font-blog text-3xl">Welcome to the Raffle!</h1>
+      <div className="ml-auto py-2 px-4">
+        <ConnectButton moralisAuth={false} />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import ManualHeader from "../components/ManualHeader";
+import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -15,7 +16,7 @@ const Home: NextPage = () => {
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <ManualHeader />
+      <Header />
     </div>
   );
 };
