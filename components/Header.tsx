ec12fd47ea6d1d899e4d6c9670520075642e2f38
mme <9083787+mme022@users.noreply.github.com>
2022-08-18 21:05:28 +0100

    added notifications and a test button for performUpkeep

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,14 @@
 import { NextPage } from "next";
 import { ConnectButton } from "@web3uikit/web3";
+import { CryptoLogos } from "@web3uikit/core";
 
 const Header: NextPage = () => {
   return (
     <div className="p-5 border-b-2 flex flex-row">
-      <h1 className="py-4 px-4 font-blog text-3xl">Welcome to the Raffle!</h1>
+      <CryptoLogos chain="ethereum" size="48px" />
+      <h1 className="py-4 px-4 mx-auto font-blog text-3xl">
+        Welcome to the Raffle!
+      </h1>
       <div className="ml-auto py-2 px-4">
         <ConnectButton moralisAuth={false} />
       </div>
--- components/RaffleEntrance.tsx
+++ components/RaffleEntrance.tsx
@@ -2,8 +2,14 @@ import { NextPage } from "next";
 import { useWeb3Contract, useMoralis } from "react-moralis";
 import { parseEther } from "ethers/lib/utils";
 import { contractAddresses, abi } from "../constants/index";
-import { useEffect, useState } from "react";
+import {
+  JSXElementConstructor,
+  ReactElement,
+  useEffect,
+  useState,
+} from "react";
 import { BigNumber, ethers } from "ethers";
+import { IPosition, notifyType, useNotification } from "@web3uikit/core";
 
 interface contractAddressesInterface {
   [key: string]: string[];
@@ -15,6 +21,7 @@ const RaffleEntrance: NextPage = () => {
   const addressArr: contractAddressesInterface = contractAddresses;
   const raffleAddress = chainId in addressArr ? addressArr[chainId][0] : null;
   const [entranceFee, setEntranceFee] = useState("0");
+  const dispatch = useNotification();
 
   const { runContractFunction: enterRaffle } = useWeb3Contract({
     abi: abi,
@@ -31,22 +38,88 @@ const RaffleEntrance: NextPage = () => {
       functionName: "getEntranceFee",
       params: {},
     });
-  entranceFeeError ? console.log(entranceFeeError) : null;
+
+  const { runContractFunction: performUpkeep, error: performUpkeepError } =
+    useWeb3Contract({
+      abi: abi,
+      contractAddress: raffleAddress!,
+      functionName: "performUpkeep",
+      params: {},
+    });
 
   async function updateUI() {
     const entranceFeeFromCall = (
       (await getEntranceFee()) as BigNumber
     ).toString();
-    setEntranceFee(ethers.utils.formatUnits(entranceFeeFromCall, "ether"));
+    setEntranceFee(entranceFeeFromCall);
   }
+
+  async function handleSuccess(tx) {
+    console.log("success");
+    await tx.wait(1);
+    handleNewNotification("info", null, "topR");
+  }
+
+  const handleNewNotification = function (
+    type: notifyType,
+    icon?: React.ReactElement,
+    position?: IPosition
+  ) {
+    dispatch({
+      type,
+      message: "Transaction completed!",
+      title: "Transaction Notification",
+      icon,
+      position: position || "topR",
+    });
+  };
 
   useEffect(() => {
     if (isWeb3Enabled) {
       updateUI();
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isWeb3Enabled]);
 
-  return <div className="py-5">Entrance fee is {entranceFee} eth</div>;
+  return (
+    <div className="p-5 flex flex-row">
+      {entranceFee ? (
+        <div className="px-4">
+          <p className="pb-2">
+            The entrance fee is {ethers.utils.formatUnits(entranceFee, "ether")}{" "}
+            eth
+          </p>
+          <button
+            className="bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            onClick={async () =>
+              await enterRaffle({
+                onSuccess: (results) => handleSuccess(results),
+                onError: (error) => {
+                  console.log(error);
+                },
+              })
+            }
+          >
+            Enter Raffle
+          </button>
+          <button
+            className="bg-red-600 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            onClick={async () =>
+              await performUpkeep({
+                onError: (error) => {
+                  console.log(error);
+                },
+              })
+            }
+          >
+            perform upkeep
+          </button>
+        </div>
+      ) : (
+        <div>No Raffle contract detected!</div>
+      )}
+    </div>
+  );
 };
 
 export default RaffleEntrance;
