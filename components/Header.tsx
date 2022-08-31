a7a4eff7b65425a1bdbf4836d102aefbbeee479f
mme <9083787+mme022@users.noreply.github.com>
2022-08-31 11:49:04 +0100

    moved h1 in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,7 @@ const Header: NextPage = () => {
   return (
     <div className="p-5 border-b-2 flex flex-row">
       <CryptoLogos chain="ethereum" size="48px" />
-      <h1 className="py-4 px-4 mx-auto font-blog text-3xl">
+      <h1 className="py-4 px-4 ml-auto font-blog text-3xl">
         Welcome to the Raffle!
       </h1>
       <div className="ml-auto py-2 px-4">
